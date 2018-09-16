dbbf059cb5db1ea64187062dee08303be38c6ce9
Mark Zsombor <markszsombor@gmail.com>
2018-09-15 21:38:27 -0700

    added documentation

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,10 +3,18 @@ defmodule Identicon do
   Creates an Identicon (a 5x5 image file) based on a string input.
   """
 
+  @doc """
+    Responsible for chaining the needed functions
+  """
+
   def main(input) do
     input
     |> hash_input
   end
+  
+  @doc """
+    Converts a string into a list of numbers
+  """
   
   def hash_input(input) do
     :crypto.hash(:md5, input)
