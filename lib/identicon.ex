e7c8eb085edeb9373a490c47ade56daebfe1ea1d
Mark Zsombor <markszsombor@gmail.com>
2018-09-16 13:44:25 -0700

    added image struct and pick color function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,6 +10,7 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
   
   @doc """
@@ -17,8 +18,19 @@ defmodule Identicon do
   """
   
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    seed = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{seed: seed}
+  end
+
+  @doc """
+    Determins a RGB color from the first three values of a struct input
+  """
+
+  # Pattern matching can be done in the argument of the function
+  def pick_color(%Identicon.Image{seed: [r, g, b | _]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
 end
